34336799adb42e53d985fbd8f3be5f3c585fc89e
EvgeniiBuslaev <eugenebus144@gmail.com>
2024-01-06 17:26:29 +0500

    Настроил доступ к env переменным на сервере

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv').config()
 
 app.get('/', (req, res) => {
     res.send('Hello, Express!');
 });
 
-const port = 3001;
+const port = dotenv.parsed?.PORT_SERVER || 3001;
+console.log(dotenv)
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
