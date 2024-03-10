0eaec74c4b244781c6a174e3b60a48bf2246201a
Evgenii <evgeniy.buslaev@elementaree.ru>
2024-03-10 13:36:04 +0500

    Created basic webpack config file

---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: {
+            neuroCare: path.resolve(__dirname, 'src', 'index.js')
+        },
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        }
+    }
+}
