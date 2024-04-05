ad9f350fee70c6eaa3ce5e40b8684d64c06c3d19
Evgenii <evgeniy.buslaev@elementaree.ru>
2024-04-05 19:37:05 +0500

    Added css modules isolation by hash

--- client/config/build/loaders/index.ts
+++ client/config/build/loaders/index.ts
@@ -5,11 +5,21 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 export default function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development'
 
+    const scssLoaderWithModules = {
+
+        loader: "css-loader",
+        options: {
+            modules: {
+                localIdentName: isDev ? "[path][name]__[local]--[hash:base64:5]" : '[hash:base64:8]',
+            }
+        },
+    }
+
     const scssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            "css-loader",
+            scssLoaderWithModules,
             "sass-loader",
             "postcss-loader"
         ],
