02fda811c5e013d2eeef729648640f6befa40085
eugenebus <evgeniy.buslaev@elementaree.ru>
2024-04-27 19:12:53 +0500

    Added favicon and copy webpack plugin

--- client/config/build/types/index.ts
+++ client/config/build/types/index.ts
@@ -4,6 +4,7 @@ export interface BuildPaths {
     entry: string,
     html: string,
     output: string,
+    public: string,
     src: string,
 }
 export interface BuildOptions {
--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -17,6 +17,7 @@ export default (env: IEnvVariables) => {
         output: path.resolve(__dirname, 'build'),
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         html: path.resolve(__dirname, 'public', 'index.html'),
+        public: path.resolve(__dirname, 'public'),
         src: path.resolve(__dirname, 'src')
     }
     const config: WebpackConfiguration = buildWebpack({
